5625b1f5eb9d97d21df717400ec3e659b551e508
hatysfriend <hatysfriend@gmail.com>
2021-01-18 18:29:26 +1100

    Merge changes - Sidebar, pageComponent

--- src/components/PageComponent.jsx
+++ src/components/PageComponent.jsx
@@ -1,15 +1,33 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { Component, useState, useEffect, useRef } from 'react';
 import '../styles/pageComponent.css'
+export default class PageComponent extends Component {
 
-export default function PageComponent(){
-    const [pageList, setPageList] = useState('pageData');
+  state=[]
+    // constructor(id, name) {
 
-      return (
-          <div className="PageComponent">
+    constructor(){
+        super();
+        this.state=[{
+            pageId: "846u9qm",
+            pageName: "React",
+            data: "1",
+            file_inside: [<PageComponent></PageComponent>]
+          }]
+    }
+    getName=()=>{
+        return this.state.pageName;
+         }
+    
+
+    render() {
+         
+        return (
+            <div className="PageComponent">
               <h1><span className="headerContentArea" contenteditable="true" data-placeholder="Untitled"></span></h1>
               <span className="mainContentArea" contenteditable="true" data-placeholder="Type '/' for commands"></span>
           </div>
-      )
+        )
+    }
 }
 
 
