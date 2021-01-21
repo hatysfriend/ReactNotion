c9b55e6c179c7910990ed3ee924b79f3326c6342
hatysfriend <hatysfriend@gmail.com>
2021-01-21 18:08:27 +1100

    Working on PageComponent onClick toggle Modal

--- src/components/PageComponent.jsx
+++ src/components/PageComponent.jsx
@@ -1,40 +1,45 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import '../styles/pageComponent.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import Toggle from './Toggle';
+import Toggle from './ToggleComponent';
 
 export default class PageComponent extends Component {
     render() {
         return (
             <div className="PageComponent">
                 <h1><span className="headerContentArea" contentEditable="true" data-placeholder="Untitled"></span></h1>
-                
+
                 <span className="mainContentArea">
                     <div className="contentButtons">
-                    
-                    <Toggle>
-                    {
-                        ({on, toggle}) => (
-                            <React.Fragment>
-                                { (on) && (<h1>SHOW ME</h1>) }
-                                <button onClick={toggle}>Show/hide</button>
-                            </React.Fragment>
-                        )
-                    } 
-                    </Toggle>
-
-                        <FontAwesomeIcon className="addContentButton" icon="plus"/>
-                        <div className="settingsContentButton">
-                            <FontAwesomeIcon icon="ellipsis-v"/>
-                            <FontAwesomeIcon icon="ellipsis-v"/>
-                        </div>
-                        
+                        <Toggle>
+                            {
+                                ({ on, toggle }) => (
+                                    <React.Fragment>
+                                        { (on) && (<div>Content Modal Goes Here...</div>)}
+                                        <div onClick={toggle}><FontAwesomeIcon className="addContentButton" icon="plus" /></div>
+                                    </React.Fragment>
+                                )
+                            }
+                        </Toggle>
+                        <Toggle>
+                            {
+                                ({ on, toggle }) => (
+                                    <React.Fragment className="settingsMenu">
+                                        { (on) && (<div>SETTINGS Modal Goes Here...</div>)}
+                                        <div onClick={toggle} className="settingsContentButton">
+                                            <FontAwesomeIcon icon="ellipsis-v" />
+                                            <FontAwesomeIcon icon="ellipsis-v" />
+                                        </div>
+                                    </React.Fragment>
+                                )
+                            }
+                        </Toggle>
                         <span className="contentArea" contentEditable="true" data-placeholder="Type '/' for commands"></span>
                     </div>
                 </span>
             </div>
         )
-    }   
+    }
 }
 
--- src/components/ToggleComponent.jsx
+++ src/components/ToggleComponent.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class Toggle extends Component {
+export default class ToggleComponent extends Component {
   state = {
     on:false,
   }
